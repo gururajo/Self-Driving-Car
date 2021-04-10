a7a7a10b7590b0184e38d2472621d62e7c36c422
gururajo <gururajotagerimail2@gmail.com>
2021-04-10 19:19:04 +0530

    added classcounter

--- tests/collecting_data/imagesclasscounter.py
+++ tests/collecting_data/imagesclasscounter.py
@@ -8,11 +8,11 @@ datacounter=len(datalist)+1
 for i in range(len(datalist)):
     data=np.load("DATA/"+datalist[i],allow_pickle=True)
     print("diving this",datalist[i])
-    
+    data=data['arr_0']
     for fna in data:
-        angle=fna[1]*515+1285
+        angle=float(fna[1])*515+1285
         index = int((1800-int(angle))/10)
         anglelist[index]+=1
     print(anglelist,len(data))
     data=0
-print(np.average(anglelist),max(anglelist),min(anglelist))
+print("mean",np.average(anglelist),"\nmax=",max(anglelist),"\nmin=",min(anglelist))
--- tests/temp/test.py
+++ tests/temp/test.py
@@ -1,9 +1,17 @@
-mylist=list(range(105))
-# for i in range(len(mylist)):
-#     print(i)
-#     if mylist[i]%10==0:
-#         mylist.pop(i)
-
-print(mylist)
-nmylist=list(range(-40,400))
-newlist=nmylist-mylist
+from tkinter import * 
+angle=1540
+def assign_angle(val):
+    global angle
+    print(val)
+    angle = val
+    # print(val)
+def butted():
+    print("pressed")
+def quitfunc():
+    print("quit")
+root = Tk()
+root.geometry('600x200')
+slider1 = Scale(root, from_=1285, to=1800, length=400, resolution=1,orient=HORIZONTAL, command=assign_angle, variable=angle).pack()
+quitme=Button(root,command=quitfunc,text="QUIT",height=3,width=10).pack(side=LEFT,padx=100)
+pauseorresume=Button(root,command=butted,text="pause/resume",height=3,width=10).pack(side=RIGHT,padx=100)
+root.mainloop()
