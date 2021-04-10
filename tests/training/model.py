5f96ae94773fde68a9240c8f59e14bb1beb63a7c
gururajo <gururajotagerimail2@gmail.com>
2021-04-10 19:50:29 +0530

    added model

---
+++ tests/training/model.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+def getmodel():
+    model = Sequential()
+
+    model.add(Conv2D(filters=24, kernel_size=3,strides=(2,2),input_shape=(66,200, 3), activation='relu'))
+    
+    model.add(Conv2D(filters=36, kernel_size=3,strides=(2,2),activation='relu'))
+    
+    model.add(Conv2D(filters=48, kernel_size=3,strides=(2,2),activation='relu'))
+    
+
+    model.add(Conv2D(filters=64, kernel_size=3,strides=(2,2),activation='relu'))#1
+    
+    model.add(Conv2D(filters=64, kernel_size=3,strides=(2,2),activation='relu'))#22
+    
+    model.add(Flatten())
+    
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
+
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
