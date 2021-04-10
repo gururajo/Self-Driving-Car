7e4258aa0cc52e2835511dcd893ad5c6bec76221
gururajo <gururajotagerimail2@gmail.com>
2021-04-10 20:31:19 +0530

    hmm

--- tests/training/modell.py
+++ tests/training/modell.py
@@ -1,31 +1,5 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-def getmodel():
-    model = Sequential()
-
-    model.add(Conv2D(filters=24, kernel_size=3,strides=2,input_shape=(66,200, 3), activation='relu'))
-    
-    model.add(Conv2D(filters=36, kernel_size=3,strides=2,activation='relu'))
-    
-    model.add(Conv2D(filters=48, kernel_size=3,strides=2,activation='relu'))
-    
-
-    model.add(Conv2D(filters=64, kernel_size=3,strides=2,activation='relu'))#1
-    
-    model.add(Conv2D(filters=64, kernel_size=3,strides=2,activation='relu'))#22
-    
-    model.add(Flatten())
-    
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
-    model.add(Dense(1))
-
-
-    model.compile(loss='mean square error',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    return model
 def getanothermodel():
     model = Sequential()
 
@@ -49,6 +23,33 @@ def getanothermodel():
     model.add(Dense(50, activation='relu'))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1, activation='softmax'))
+
+
+    model.compile(loss='mean square error',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
+
+def getmodel():
+    model = Sequential()
+
+    model.add(Conv2D(filters=24, kernel_size=3,strides=2,input_shape=(66,200, 3), activation='relu'))
+    
+    model.add(Conv2D(filters=36, kernel_size=3,strides=2,activation='relu'))
+    
+    model.add(Conv2D(filters=48, kernel_size=3,strides=2,activation='relu'))
+    
+
+    model.add(Conv2D(filters=64, kernel_size=3,strides=2,activation='relu'))#1
+    
+    model.add(Conv2D(filters=64, kernel_size=3,strides=2,activation='relu'))#22
+    
+    model.add(Flatten())
+    
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
 
 
     model.compile(loss='mean square error',
